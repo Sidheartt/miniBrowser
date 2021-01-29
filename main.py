46a3dcc71bda681c9e94b372874dd499afd1c45f
Siddharth <siddharth.desai1996@gmail.com>
2021-01-29 13:50:32 +0530

    Making a spirograph with turtle (making it stop with the help of gap size)

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
-from turtle import Turtle, Screen
+import turtle as t
 import random
 
-tim = Turtle()
-print(tim)
+tim = t
 tim.shape("turtle")
-
-colors = ["orange", "maroon", "cyan", "yellow", "navy"]
-
-
-def draw(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
+t.colormode(255)
+tim.speed("fastest")
 
 
-for side in range(3, 11):
-    tim.color(random.choice(colors))
-    draw(side)
+def rand_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    ra = (r, g, b)
+    return ra
 
 
-S = Screen()
-S.exitonclick()
+def draw_spirograph(size_of_gap):
+    for i in range(int(360/size_of_gap)):
+        tim.color(rand_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+
+draw_spirograph(5)
+
+screen = t.Screen()
+screen.exitonclick()
+
 
